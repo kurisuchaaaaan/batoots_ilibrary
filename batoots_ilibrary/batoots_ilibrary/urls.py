00d15475e1a9bde4e02313fe08377f175b763274
kurisuchaaaaan <kurisuchaaaaan@gmail.com>
2019-05-31 16:19:08 +0800

    done with display

--- batoots_ilibrary/batoots_ilibrary/urls.py
+++ batoots_ilibrary/batoots_ilibrary/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from bookcollections.views import BookListView, BookDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', BookListView.as_view()),
 ]
