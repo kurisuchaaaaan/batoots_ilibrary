3727ba271b2a7606b0ff350c269905b16b75f314
Ben Cariazo <bcariazo@gbox.adnu.edu.ph>
2019-05-31 16:18:36 +0800

    Modifies urls.py and views.py

--- batoots_ilibrary/batoots_ilibrary/urls.py
+++ batoots_ilibrary/batoots_ilibrary/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from animals.views import HomePageView, DogListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', HomePageView.as_view()),
+    path('books/', BookListView.as_view()),
