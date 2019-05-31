f2fb5fa9596a5c0a0f7752bf5a2ef568ec6d7644
Ben Cariazo <bcariazo@gbox.adnu.edu.ph>
2019-05-31 16:00:17 +0800

    added home.html

--- batoots_ilibrary/bookcollections/models.py
+++ batoots_ilibrary/bookcollections/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    DOG_COLORS = (("br", "Brown"),
+                ("bl", "Black"),
+                ("wh", "White"),
+                ("go", "Gold")
+                )
+    Title = models.CharField("Breed", max_length=100)
+    birth_date = models.DateTimeField("Birth Date", \
+         null = True, blank = True)
+    color = models.CharField("Color", max_length=2, \
+        choices=DOG_COLORS)
+    unique_features = models.TextField("Unique Features",
+        max_length=1000)
+        
+    def __str__(self):
+        return "{} {}".format(self.color, self.breed)
